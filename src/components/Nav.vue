b649c6aa7f4a68f45a83277750fd75f74c6887e7
Yibo-Hao <sxyhao@hotmail.com>
2020-03-21 23:52:05 +0800

    add icons into nav components

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,38 @@
 <template>
   <div>
-    <router-link to="/money">money</router-link>
-    |
-    <router-link to="/labels">labels</router-link>
-    |
-    <router-link to="/statistic">statistic</router-link>
+    <router-link to="/money">
+      <svg>
+        <use xlink:href="#money"></use>
+      </svg>
+    </router-link>
+
+    <router-link to="/labels">
+      <svg>
+        <use xlink:href="#labels"></use>
+      </svg>
+      labels
+    </router-link>
+
+    <router-link to="/statistic">
+      <svg>
+        <use xlink:href="#statistic"></use>
+      </svg>
+      statistic
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
+import x from "@/assets/icons/labels.svg";
+import y from "@/assets/icons/money.svg";
+import z from "@/assets/icons/statistic.svg";
+
+console.log(y);
+console.log(x);
+console.log(z);
+
 export default {
-  "name": "Nav"
+  name: "Nav"
 };
 </script>
 
