593ad31839a902b7b6119a9749f1a38c93fb56b2
Yibo-Hao <sxyhao@hotmail.com>
2020-03-25 13:41:55 +0800

    finished the notes.vue

--- src/components/money/Notes.vue
+++ src/components/money/Notes.vue
@@ -1,16 +1,25 @@
 <template>
   <div>
+    {{ value }}
     <label class="notes">
       <span class="name">备注:</span>
-      <input type="text" placeholder="写点备注吧~" />
+      <input
+        type="text"
+        v-model="value"
+        placeholder="写点备注吧~"
+      />
     </label>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-  name: "Notes"
-};
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component
+export default class Notes extends Vue {
+  value = "";
+}
 </script>
 
 <style lang="scss" scoped>
